bca8396d824d68a2128e9b9908ba9f172a91c76b
shakthy <shakthy@gmail.com>
2020-04-12 19:50:17 +0100

    commit2

---
+++ gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,19 @@
+package guru99.gittest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class AppTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
